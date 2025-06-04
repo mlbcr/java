f1ae2a40dca404b4d18b2c16d4a34d6265539b67
Maria Luísa Barreto de Campos Ribeiro <malubacari@gmail.com>
2025-06-04 10:10:47 -0300

    created a oop bank account

---
+++ untitled/src/banco/Conta.java
@@ -0,0 +1,42 @@
+package banco;
+
+public class Conta {
+    private int num_conta;
+    private String titular;
+    private double balanco;
+
+
+    public Conta(int num_conta, String titular, double valor_inicial){
+        this.num_conta = num_conta;
+        this.titular = titular;
+        this.balanco = valor_inicial;
+    }
+
+    public int getNum_conta(){
+        return num_conta;
+    }
+
+    public String getTitular(){
+        return titular;
+    }
+    public void setTitular(String titular){
+        this.titular = titular;
+    }
+
+    public double getBalanco(){
+        return balanco;
+    }
+
+    public void mostrarConta(){
+        System.out.printf("Dados da conta:\nConta %d, Titular: %s, Balanço: R$%.2f\n",
+                this.getNum_conta(), this.getTitular(), this.getBalanco());
+    }
+
+    public void realizarDeposito(double novo_deposito){
+        this.balanco += novo_deposito;
+    }
+
+    public void realizarSaque(double novo_saque){
+        this.balanco -= (novo_saque + 5);
+    }
+}
---
+++ untitled/src/banco/Main.java
@@ -0,0 +1,45 @@
+package banco;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        double deposito_inicial;
+
+        System.out.print("Insira o número da conta: ");
+        int num_conta = input.nextInt();
+
+        input.nextLine();
+
+        System.out.print("Insira o titular da conta: ");
+        String titular = input.nextLine();
+
+        System.out.print("Existe um depósito inicial (s/n)? ");
+        char resp = input.next().charAt(0);
+
+        if (resp == 's'){
+            System.out.print("Insira o valor do depósito inicial: ");
+            deposito_inicial = input.nextDouble();
+        } else {
+            deposito_inicial = 0;
+        }
+
+        Conta conta = new Conta(num_conta, titular, deposito_inicial);
+
+        input.nextLine();
+        conta.mostrarConta();
+
+        System.out.print("\nInsira um valor de depósito: ");
+        double novo_deposito = input.nextDouble();
+
+        conta.realizarDeposito(novo_deposito);
+        conta.mostrarConta();
+
+        System.out.print("\nInsira um valor de depósito: ");
+        double novo_saque= input.nextDouble();
+
+        conta.realizarSaque(novo_saque);
+        conta.mostrarConta();
+    }
+}
